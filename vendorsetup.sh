cb8602fa9c1d9544884155605db006b1fac60a94
kamleshhv <kamleshvansajalia@gmail.com>
2024-07-19 04:10:46 +0000

    sweet2: track repo with vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone -b 14 https://github.com/kamleshhv/device_xiaomi_sm6150-common-sweet2.git device/xiaomi/sm6150-common
+git clone -b 14 https://github.com/kamleshhv/vendor_xiaomi_sm6150-common-sweet2.git vendor/xiaomi/sm6150-common
+git clone -b 14 https://github.com/kamleshhv/vendor_xiaomi_sweet2.git vendor/xiaomi/sweet2
+git clone -b lineage-21 --depth=1 https://github.com/Positron-B/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
